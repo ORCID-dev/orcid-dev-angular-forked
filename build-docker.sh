1a496275ddb963dde3aa6ebe71bffa16fa920bce
Giles Westwood <giles.westwood@gmail.com>
2024-11-26 00:03:47 +0000

    sync build from main repo

--- build-docker.sh
+++ build-docker.sh
@@ -2,4 +2,7 @@
 
 # debugging params
 # docker compose -f docker-compose.build.yml build --no-cache --progress=plain
-docker compose -f docker-compose.build.yml build
+
+export DOCKER_BUILDKIT=1
+
+docker compose -f docker-compose.build.yarn.yml build --no-cache --progress=plain
